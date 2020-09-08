2b00e02406e2513b5b94b0df4fb377324311f2b9
DevasishMahato <t7.devasishmahato@gmail.com>
2020-09-09 00:30:11 +0530

    updated code for manhattan beach scraper- for every category

--- manhattanBeach_scraper/scraper_ec2.py
+++ manhattanBeach_scraper/scraper_ec2.py
@@ -99,9 +99,9 @@ class Scraper:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument('--disable-dev-shm-usage')
-        # self.driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
-        self.driver = webdriver.Chrome('E:/Codes/chromedriver.exe',chrome_options=chrome_options)
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
+        # self.driver = webdriver.Chrome('E:/Codes/chromedriver.exe',chrome_options=chrome_options)
 
         with switch_collection(MB_scraper, 'manhattanBeach_scraper') as MB_scraper:
             url = "https://business.manhattanbeachchamber.com/members"
