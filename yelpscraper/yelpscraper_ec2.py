f906ecd754f60707d08738bd210fe739d4f44a1d
DevasishMahato <t7.devasishmahato@gmail.com>
2020-09-18 01:32:23 +0530

    yelp address upgraded

--- yelpscraper/yelpscraper_ec2.py
+++ yelpscraper/yelpscraper_ec2.py
@@ -128,9 +128,9 @@ class Scraper:
             chrome_options.add_argument("--headless")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.add_argument("--disable-gpu")
-            # chrome_options.add_argument('--disable-dev-shm-usage')
-            # driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
-            driver = webdriver.Chrome('E:/Codes/chromedriver.exe')#,chrome_options=chrome_options)
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
+            # driver = webdriver.Chrome('E:/Codes/chromedriver.exe')#,chrome_options=chrome_options)
             try:
                 with switch_collection(MB_scraper, 'yelpscrapermailinglist') as MB_scraper:
                     MB_scraper.objects(user_id = self.userid, name = self.name).update(set__limit = self.limit)
